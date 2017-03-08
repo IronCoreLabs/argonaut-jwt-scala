9a11fa23603b1865bf0f5ac7148fba6eeed77418
Colt Frederickson <coltfred@gmail.com>
2017-03-08 10:11:22 -0700

    Use TryValues instead of EitherValues for compat with 2.11

--- src/test/scala/com/ironcorelabs/jwt/P256VerifierTest.scala
+++ src/test/scala/com/ironcorelabs/jwt/P256VerifierTest.scala
@@ -1,14 +1,14 @@
 package com.ironcorelabs.jwt
 
-import org.scalatest.{WordSpec, Matchers, EitherValues}
+import org.scalatest.{WordSpec, Matchers, TryValues}
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey
 import scodec.bits._
 
-class P256PublicKeyVerifierTest extends WordSpec with EitherValues with Matchers {
+class P256PublicKeyVerifierTest extends WordSpec with TryValues with Matchers {
   "P256PublicKeyVerifier" should {
     "successfully convert valid public key with a 30 byte x" in {
       val (x, y) = hex"6c8bdf3cbbab30bff5467539ac462df79583f17aa828983f45c3d04c55ab" -> hex"00b213df136f2c7a987580e1bd0251b682cb13572488d27d4c9a148d9e1e7f0e6d"
-      val publicKey = P256PublicKeyVerifier(x, y).toEither.right.value
+      val publicKey = P256PublicKeyVerifier(x, y).success.value
       publicKey shouldBe a[BCECPublicKey]
       val qPoint = publicKey.asInstanceOf[BCECPublicKey].getQ
       val xResult = ByteVector.view(qPoint.getAffineXCoord().toBigInteger().toByteArray)
@@ -20,7 +20,7 @@ class P256PublicKeyVerifierTest extends WordSpec with EitherValues with Matchers
     "fail for negative number which isn't on the curve" in {
       //8b causes it to fail because it doesn't have a leading 0 bit so it's negative
       val (x, y) = hex"8b" -> hex"12"
-      val error = P256PublicKeyVerifier(x, y).toEither.left.value
+      val error = P256PublicKeyVerifier(x, y).failure.exception
       //Note that because the verifier pads the 0s onto the front it doesn't fail because of the exception thrown in Nat256 (in bouncy castle)
       error shouldBe an[PointNotOnCurve]
     }
@@ -28,7 +28,7 @@ class P256PublicKeyVerifierTest extends WordSpec with EitherValues with Matchers
     "fail for point not on curve" in {
       //Changed the first char of x to 5 from a 6
       val (x, y) = hex"5c8bdf3cbbab30bff5467539ac462df79583f17aa828983f45c3d04c55ab" -> hex"00b213df136f2c7a987580e1bd0251b682cb13572488d27d4c9a148d9e1e7f0e6d"
-      val error = P256PublicKeyVerifier(x, y).toEither.left.value
+      val error = P256PublicKeyVerifier(x, y).failure.exception
       error shouldBe an[PointNotOnCurve]
       val PointNotOnCurve(errorX, errorY) = error.asInstanceOf[PointNotOnCurve]
       errorX shouldBe x
@@ -38,7 +38,7 @@ class P256PublicKeyVerifierTest extends WordSpec with EitherValues with Matchers
     "fail for point which is too big" in {
       //Changed the first char of x to 5 from a 6
       val (x, y) = ByteVector.concat(List.fill(33)(hex"25")) -> hex"00"
-      val error = P256PublicKeyVerifier(x, y).toEither.left.value
+      val error = P256PublicKeyVerifier(x, y).failure.exception
       error shouldBe an[IllegalArgumentException] //Key was too large, see Nat256.fromBigInteger
     }
   }
